f2d73f425c9d3b7fb9251b3c608d02598297cb33
Justin Gu <justingu102@gmail.com>
2024-11-28 01:54:03 +0000

    final iteration 1

--- madlibs.c
+++ madlibs.c
@@ -9,51 +9,67 @@
 #define NUM_NOUNS 10
 #define NUM_VERBS 10
 #define NUM_ADJECTIVES 10
+#define LINE_CAP 100
 
-void scanFile(FILE* userFilePTR, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex);
+//Reading files and printing result functions
+void scanFile(FILE* userFilePTR, char scannedFile[][STR_CAP], int* numLinesPTR, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex);
+void printWords(FILE* userFilePTR, char scannedFile[][STR_CAP], int numLines, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex);
+
+//Placeholder reader functions
 void nounInput(char nouns[][STR_CAP], int* nounIndex);
 void verbInput(char verbs[][STR_CAP], int* verbIndex);
 void adjectiveInput(char adjectives[][STR_CAP], int* adjectiveIndex);
 
-void printWords(FILE* userFilePTR, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex);
+//String functions
 int stringLength(char str[]);
 void stringCopy(char[], char[]);
 
 int main(){
 	char userFile[USERFILESIZE];
 	char nouns[NUM_NOUNS][STR_CAP], verbs[NUM_VERBS][STR_CAP], adjectives[NUM_ADJECTIVES][STR_CAP];
-	int nounIndex = 0, verbIndex = 0, adjectiveIndex = 0;
+	int nounIndex = 0, verbIndex = 0, adjectiveIndex = 0, numLines = 0;
+	char scannedFile[LINE_CAP][STR_CAP];
 	
+	//Prompt user for madlibs file
 	printf("Please enter desired madlibs file (madlib1.txt, madlib2.txt): ");
 	fgets(userFile, USERFILESIZE, stdin);
 	userFile[stringLength(userFile) - 1] = '\0';
 	
+	//Opening and checking if process was successful
 	FILE* userFilePTR = fopen(userFile, "r");
 	if(userFilePTR == NULL){
 		printf("Could not open %s, please try again.\n", userFile);
 		return 0;
 	}
 	
-	scanFile(userFilePTR, nouns, &nounIndex, verbs, &verbIndex, adjectives, &adjectiveIndex);
+	//Scanning file for placeholders
+	scanFile(userFilePTR, scannedFile, &numLines, nouns, &nounIndex, verbs, &verbIndex, adjectives, &adjectiveIndex);
+	
+	//Reset indexs for placeholders
 	nounIndex = 0;
 	verbIndex = 0;
 	adjectiveIndex = 0;
-	
+	 
+	//Reset cursor for file
 	fclose(userFilePTR);
-	fopen(userFile, "r");
-	printWords(userFilePTR, nouns, &nounIndex, verbs, &verbIndex, adjectives, &adjectiveIndex);
+	userFilePTR = fopen(userFile, "r");
+	if(userFilePTR == NULL){
+		printf("Could not open %s again, please try again.\n", userFile);
+		return 0;
+	}
 	
+	//Print final results
+	printWords(userFilePTR, scannedFile, numLines, nouns, &nounIndex, verbs, &verbIndex, adjectives, &adjectiveIndex);
 	fclose(userFilePTR);
-	
 	
 	return 0;
 }
 
-void scanFile(FILE* userFilePTR, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex){
+void scanFile(FILE* userFilePTR, char scannedFile[][STR_CAP], int* numLinesPTR, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex){
 	char scannedLine[STR_CAP];
 	while(fgets(scannedLine, STR_CAP, userFilePTR) != NULL){
-		if(stringLength(scannedLine) == 2){
-			switch(scannedLine[0]){
+		if(stringLength(scannedLine) == 2){ //Check if line length is 2, i.e. "V\n"
+			switch(scannedLine[0]){ //Check the first character of line and run the corresponding function
 				case 'A':
 					adjectiveInput(adjectives, adjectiveIndex);
 					break;
@@ -65,63 +81,80 @@ void scanFile(FILE* userFilePTR, char nouns[][STR_CAP], int* nounIndex, char ver
 					break;	
 			}
 		}
+		
+		//Copy current string into an array of strings for the file
+		stringCopy(scannedLine, scannedFile[*numLinesPTR]);
+		(*numLinesPTR)++;
 	}
-	
 }
 
 void nounInput(char nouns[][STR_CAP], int* nounIndex){
 	printf("Please enter a noun: ");
 	fgets(nouns[*nounIndex], STR_CAP, stdin);
-	nouns[*nounIndex][stringLength(nouns[*nounIndex]) - 1] = '\0';
+	nouns[*nounIndex][stringLength(nouns[*nounIndex]) - 1] = '\0'; //Setting endline for noun string
 	(*nounIndex)++;
-
 }
 
 void verbInput(char verbs[][STR_CAP], int* verbIndex){
 	printf("Please enter a verb: ");
 	fgets(verbs[*verbIndex], STR_CAP, stdin);
-	verbs[*verbIndex][stringLength(verbs[*verbIndex]) - 1] = '\0';
+	verbs[*verbIndex][stringLength(verbs[*verbIndex]) - 1] = '\0'; //Setting endline for verb string
 	(*verbIndex)++;
-
 }
 
 void adjectiveInput(char adjectives[][STR_CAP], int* adjectiveIndex){
 	printf("Please enter a adjective: ");
 	fgets(adjectives[*adjectiveIndex], STR_CAP, stdin);
-	adjectives[*adjectiveIndex][stringLength(adjectives[*adjectiveIndex]) - 1] = '\0';
+	adjectives[*adjectiveIndex][stringLength(adjectives[*adjectiveIndex]) - 1] = '\0'; //Setting endline for adjective string
 	(*adjectiveIndex)++;
-
 }
 
 
-void printWords(FILE* userFilePTR, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex){
+void printWords(FILE* userFilePTR, char scannedFile[][STR_CAP], int numLines, char nouns[][STR_CAP], int* nounIndex, char verbs[NUM_VERBS][STR_CAP], int* verbIndex, char adjectives[NUM_ADJECTIVES][STR_CAP], int* adjectiveIndex){
 	char scannedLine[STR_CAP];
 	int size = 0;
-	while(fgets(scannedLine, STR_CAP, userFilePTR) != NULL){
-		if(stringLength(scannedLine) == 2){
+	for(int i = 0; i < numLines; i++){
+		stringCopy(scannedFile[i], scannedLine); //Copy current index string from array of strings to a 1D array
+		if(stringLength(scannedLine) == 2 && scannedLine[0] != '!'){ //Check if the string length is 2 and if the first character of string is not a punctuation 
 			if(scannedLine[0] == 'A'){
-				printf(" %s", adjectives[*adjectiveIndex]);
-				(*adjectiveIndex)++;		
+				if(scannedFile[i + 1][0] == '.' || scannedFile[i + 1][0] == '!' || scannedFile[i + 1][0] == ','){  //Check the next line, if it begins with a punctuation print corresponding spacing.
+					printf(" %s", adjectives[*adjectiveIndex]);
+					(*adjectiveIndex)++;	
+				} else {
+					printf(" %s ", adjectives[*adjectiveIndex]);
+					(*adjectiveIndex)++;	
+				}
 			} else if(scannedLine[0] == 'N'){
-				printf("%s" , nouns[*nounIndex]);
-				(*nounIndex)++;
+				if(scannedFile[i + 1][0] == '.' || scannedFile[i + 1][0] == '!' || scannedFile[i + 1][0] == ','){ //Check the next line, if it begins with a punctuation print corresponding spacing.
+					printf(" %s", adjectives[*adjectiveIndex]);
+					printf(" %s" , nouns[*nounIndex]);
+					(*nounIndex)++;
+				} else {
+					printf(" %s " , nouns[*nounIndex]);
+					(*nounIndex)++;
+				}
 			} else if(scannedLine[0] == 'V'){
-				printf("%s", verbs[*verbIndex]);
-				(*verbIndex)++;
+				if(scannedFile[i + 1][0] == '.' || scannedFile[i + 1][0] == '!' || scannedFile[i + 1][0] == ','){ //Check the next line, if it begins with a punctuation print corresponding spacing.
+					printf(" %s", adjectives[*adjectiveIndex]);
+					printf(" %s", verbs[*verbIndex]);
+					(*verbIndex)++;
+				} else {
+					printf(" %s ", verbs[*verbIndex]);
+					(*verbIndex)++;
+				}
 			}
 		}
 		else{
-		 	size = stringLength(scannedLine);
+			//If conditions above are not met, find the size of the current selected string, set an endline character, and print.
+		 	size = stringLength(scannedLine); 
 		 	scannedLine[size - 1] = '\0';
 		 	printf("%s", scannedLine);
 		}
-		
-		
 	}
 	printf("\n");
 }
 
-
+//STRING LIBRARY
 int stringLength(char str[]){
 	int length = 0;
 	for(int i = 0; str[i] != '\0'; i++){
@@ -138,4 +171,3 @@ void stringCopy(char source[], char dest[]){
 	}
 	dest[i] = '\0';
 }
-
